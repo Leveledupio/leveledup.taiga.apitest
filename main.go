01ed7b9d3022e8435faa7ce3d28dce634f64f954
James Strong <strong.james.e@gmail.com>
2018-02-13 20:21:20 -0500

    Added the tagia vagrant files

--- main.go
+++ main.go
@@ -1,48 +1,43 @@
 package main
 
 import (
-
 	"fmt"
-	"github.com/strongjz/taigo-test/taigo"
 
+	taigo "github.com/Leveledupio/leveledup.taiga.apitest/taigo"
 )
-
-
 
 func main() {
 
-
 	t := taigo.Project{
-		Name: "Test2",
-		Description:"words are hard",
-		KanbanAct:false,
-		IssuesAct:true,
-		BacklogAct:true,
-		Wiki:false,
-		Private:true,
+		Name:        "Test2",
+		Description: "words are hard",
+		KanbanAct:   false,
+		IssuesAct:   true,
+		BacklogAct:  true,
+		Wiki:        false,
+		Private:     true,
 	}
 
-	project,err := t.CreateProject()
+	project, err := t.CreateProject()
 	if err != nil {
 		fmt.Printf("Error creating project %s", err)
 	}
 
-
 	fmt.Printf("Project Body: %v", string(*project))
 
-/*
-	s := taigo.Story{
-		Description:"Story 2",
-		Subject:"Subject for story 2",
-		ProjectID:231729,
-	}
+	/*
+		s := taigo.Story{
+			Description:"Story 2",
+			Subject:"Subject for story 2",
+			ProjectID:231729,
+		}
 
-	err := s.CreateStory()
-	if err != nil {
-		fmt.Printf("Error creating story %s", err)
-	}
+		err := s.CreateStory()
+		if err != nil {
+			fmt.Printf("Error creating story %s", err)
+		}
 
 
-	fmt.Printf("Story ID: %v Subject%s", s.ID, s.Subject)
-*/
+		fmt.Printf("Story ID: %v Subject%s", s.ID, s.Subject)
+	*/
 }
